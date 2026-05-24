7e2f217858fda622f4212b0e2b6677bfbb8b3f79
ForestOfLight <briancassriel@gmail.com>
2026-05-24 00:22:20 -0700

    fix: /undo and /redo only accessible by OPs

--- packs/BP/scripts/commands/redo.js
+++ packs/BP/scripts/commands/redo.js
@@ -6,7 +6,7 @@ system.beforeEvents.startup.subscribe((event) => {
         name: 'nudge:redo',
         description: 'nudge.command.redo',
         optionalParameters: [{ name: 'number', type: CustomCommandParamType.Integer }],
-        permissionLevel: CommandPermissionLevel.Admin
+        permissionLevel: CommandPermissionLevel.Any
     };
     event.customCommandRegistry.registerCommand(command, redoLastEdit);
 });
